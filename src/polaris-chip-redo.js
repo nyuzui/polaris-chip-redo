82689bcd68e50ab783b2aecc5d26621fa52bd299
Maria Albakshi <mariaalbakshi@gmail.com>
2023-12-04 22:49:23 -0500

    edited

--- src/polaris-chip-redo.js
+++ src/polaris-chip-redo.js
@@ -1,86 +1,49 @@
+// polaris-chip-redo.js
 import { LitElement, html, css } from 'lit';
-
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class PolarisChipRedo extends LitElement {
   static properties = {
-    header: { type: String },
-  }
+    name: { type: String },
+    link: { type: String },
+  };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
+    .tag-module {
+      -webkit-text-size-adjust: 100%;
+      -webkit-font-smoothing: antialiased;
+      list-style: none;
+      box-sizing: inherit;
+      background-color: transparent;
+      font-family: Roboto, sans-serif;
+      font-size: 17px;
+      font-weight: 700;
+      justify-content: center;
+      letter-spacing: 0.03em;
+      line-height: 19px;
       text-align: center;
-      background-color: var(--polaris-chip-redo-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
+      text-decoration: none;
+      padding: 8px;
+      margin: 5px;
+      transition: background-color 0.3s, color 0.3s, text-decoration 0.3s, border 0.3s;
+      border: 2px solid #444;
+      color: #000;
+      display: flex;
       align-items: center;
     }
 
-    .app-footer a {
-      margin-left: 5px;
+    .tag-module:hover {
+      background-color: #f0f0f0;
+      color: #0052be;
+      text-decoration: underline;
+      border: 2px solid transparent;
     }
   `;
 
-  constructor() {
-    super();
-    this.header = 'My app';
-  }
-
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/PolarisChipRedo.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <a class="tag-module" href="${this.link}">
+        <slot>${this.name}</slot>
+      </a>
     `;
   }
 }
--- src/tag-list.js
+++ src/tag-list.js
@@ -1,5 +1,6 @@
 // tag-list.js
 import { LitElement, html, css } from 'lit';
+import './polaris-chip-redo';
 
 class TagList extends LitElement {
   static styles = css`
@@ -12,55 +13,24 @@ class TagList extends LitElement {
       display: flex;
       flex-wrap: wrap;
     }
-
-    .Block {
-      -webkit-text-size-adjust: 100%;
-      -webkit-font-smoothing: antialiased;
-      list-style: none;
-      box-sizing: inherit;
-      background-color: transparent;
-      flex: 1 0 calc(25% - 20px);
-      font-family: Roboto, sans-serif;
-      font-size: 17px;
-      font-weight: 700;
-      justify-content: center;
-      letter-spacing: 0.03em;
-      line-height: 19px;
-      text-align: center;
-      text-decoration: none;
-      padding: 8px;
-      margin: 5px;
-      transition: background-color 0.3s, color 0.3s, text-decoration 0.3s, border 0.3s;
-      border: 2px solid #444;
-      color: #000;
-      display: flex;
-      align-items: center;
-    }
-
-    .Block:hover {
-      background-color: #f0f0f0;
-      color: #0052be;
-      text-decoration: underline;
-      border: 2px solid transparent;
-    }
   `;
 
   render() {
     return html`
       <section class="Container">
         <ul class="Burbur">
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/academics >Academics</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/students>Students</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/visitors-and-neighbors>Visitors and Neighbors</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/faculty-and-staff >Faculty and Staff</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/business-and-industry>Business and Industry</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/alumni>Alumni</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/university-park>University Park</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/latest-news>Latest News</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/rankings>Rankings</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/impact>Impact</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/global>Global</a></li>
-          <li class="Block"><a class="tag-module--tag--d9ee2" href=https://www.psu.edu/news/global-programs>Global Programs</a></li>
+          <polaris-chip-redo name="Academics" link="https://www.psu.edu/news/academics"></polaris-chip-redo>
+          <polaris-chip-redo name="Students" link="https://www.psu.edu/news/students"></polaris-chip-redo>
+          <polaris-chip-redo name="Visitors and Neighbors" link="https://www.psu.edu/news/visitors-and-neighbors"></polaris-chip-redo>
+          <polaris-chip-redo name="Faculty and Staff" link="https://www.psu.edu/news/business-and-industry"></polaris-chip-redo>
+          <polaris-chip-redo name="Alumni" link="https://www.psu.edu/news/Alumni"></polaris-chip-redo>
+          <polaris-chip-redo name="University Park" link="https://www.psu.edu/news/University-Park"></polaris-chip-redo>
+          <polaris-chip-redo name="Latest News" link="https://www.psu.edu/news/Latest-News"></polaris-chip-redo>
+          <polaris-chip-redo name="Rankings" link="https://www.psu.edu/news/Rankings"></polaris-chip-redo>
+          <polaris-chip-redo name="Impact" link="https://www.psu.edu/news/Impact"></polaris-chip-redo>
+          <polaris-chip-redo name="Global" link="https://www.psu.edu/news/Global"></polaris-chip-redo>
+          <polaris-chip-redo name="Global Programs" link="https://www.psu.edu/news/Global-Programs"></polaris-chip-redo>
+
 
         </ul>
       </section>
